746799f67a7ea8b1964d52f527142a9d81753a04
Matthew Morrison <james.m.morrison@vanderbilt.edu>
2025-07-21 14:25:16 -0500

    add lazy & gruvbox

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
+-- Bootstrap lazy.nvim
+vim.opt.rtp:prepend(vim.fn.stdpath('data') .. '/lazy/lazy.nvim')
+
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<Leader>fe', ':Explore', { desc: 'Open file explorer' })
+local lazy_setup = {
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+}
+local lazy_opts = {
+	rocks = {
+		enabled = false,
+	},
+}
+require('lazy').setup(lazy_setup, lazy_opts)
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+vim.keymap.set('n', '<Leader>e', ':Explore<CR>', { desc = '[mm] Open file explorer' })
+vim.keymap.set('v', '<Leader>y', '"+y',          { desc = '[mm] Yank to clipboard' })
+vim.keymap.set('n', '<Leader>l', ':Lazy<CR>',    { desc = '[mm] Open lazy.nvim' })
